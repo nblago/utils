2d63b8d34d074b5f0d9da2616c5c5140a690f5e4
nblago <nadejda14@gmail.com>
2013-07-30 07:41:20 -0700

    Tabs fixed.

--- sdss_to_gaia_phot.py
+++ sdss_to_gaia_phot.py
@@ -10,126 +10,126 @@ This transformations are created for different values of extinction for stars of
 
 '''
 import numpy as np
-
+import os
 
 def G_g(gr=None, gi=None, gz=None, ri=None):
-'''
-	Usage: G_g(gr=sdss_gr_color)
-	
-	at least one color has to be set up.
-	
-	Input:
-	gr: SDSS g - r color
-	gi: SDSS g - i color
-	gz: SDSS g - z color
-	ri: SDSS r - i color
-'''
+	'''
+		Usage: G_g(gr=sdss_gr_color)
+		
+		at least one color has to be set up.
+		
+		Input:
+		gr: SDSS g - r color
+		gi: SDSS g - i color
+		gz: SDSS g - z color
+		ri: SDSS r - i color
+	'''
 	return getColorPos(gr, gi, gz, ri, 0)
 
 def G_Grvs(gr=None, gi=None, gz=None, ri=None):
-'''
-	Usage: G_g(gr=sdss_gr_color)
-	
-	at least one color has to be set up.
-	
-	Input:
-	gr: SDSS g - r color
-	gi: SDSS g - i color
-	gz: SDSS g - z color
-	ri: SDSS r - i color
-'''
+	'''
+		Usage: G_Grvs(gr=sdss_gr_color)
+		
+		at least one color has to be set up.
+		
+		Input:
+		gr: SDSS g - r color
+		gi: SDSS g - i color
+		gz: SDSS g - z color
+		ri: SDSS r - i color
+	'''
 	return getColorPos(gr, gi, gz, ri, 1)
 
 def G_Gbp(gr=None, gi=None, gz=None, ri=None):
-'''
-	Usage: G_g(gr=sdss_gr_color)
-	
-	at least one color has to be set up.
-	
-	Input:
-	gr: SDSS g - r color
-	gi: SDSS g - i color
-	gz: SDSS g - z color
-	ri: SDSS r - i color
-'''
+	'''
+		Usage: G_Gbp(gr=sdss_gr_color)
+		
+		at least one color has to be set up.
+		
+		Input:
+		gr: SDSS g - r color
+		gi: SDSS g - i color
+		gz: SDSS g - z color
+		ri: SDSS r - i color
+	'''
 	return getColorPos(gr, gi, gz, ri, 2)
 
 def G_Grp(gr=None, gi=None, gz=None, ri=None):
-'''
-	Usage: G_g(gr=sdss_gr_color)
-	
-	at least one color has to be set up.
-	
-	Input:
-	gr: SDSS g - r color
-	gi: SDSS g - i color
-	gz: SDSS g - z color
-	ri: SDSS r - i color
-'''
+	'''
+		Usage: G_Grp(gr=sdss_gr_color)
+		
+		at least one color has to be set up.
+		
+		Input:
+		gr: SDSS g - r color
+		gi: SDSS g - i color
+		gz: SDSS g - z color
+		ri: SDSS r - i color
+	'''
 	return getColorPos(gr, gi, gz, ri, 3)
 
 def g_Grvs(gr=None, gi=None, gz=None, ri=None):
-'''
-	Usage: G_g(gr=sdss_gr_color)
-	
-	at least one color has to be set up.
-	
-	Input:
-	gr: SDSS g - r color
-	gi: SDSS g - i color
-	gz: SDSS g - z color
-	ri: SDSS r - i color
-'''
+	'''
+		Usage: g_Grvs(gr=sdss_gr_color)
+		
+		at least one color has to be set up.
+		
+		Input:
+		gr: SDSS g - r color
+		gi: SDSS g - i color
+		gz: SDSS g - z color
+		ri: SDSS r - i color
+	'''
 	return getColorPos(gr, gi, gz, ri, 4)
 
 def g_Gbp(gr=None, gi=None, gz=None, ri=None):
-'''
-	Usage: G_g(gr=sdss_gr_color)
-	
-	at least one color has to be set up.
-	
-	Input:
-	gr: SDSS g - r color
-	gi: SDSS g - i color
-	gz: SDSS g - z color
-	ri: SDSS r - i color
-'''
+	'''
+		Usage: g_Gbp(gr=sdss_gr_color)
+		
+		at least one color has to be set up.
+		
+		Input:
+		gr: SDSS g - r color
+		gi: SDSS g - i color
+		gz: SDSS g - z color
+		ri: SDSS r - i color
+	'''
 	return getColorPos(gr, gi, gz, ri, 5)
 
 def g_Grp(gr=None, gi=None, gz=None, ri=None):
-'''
-	Usage: G_g(gr=sdss_gr_color)
-	
-	at least one color has to be set up.
-	
-	Input:
-	gr: SDSS g - r color
-	gi: SDSS g - i color
-	gz: SDSS g - z color
-	ri: SDSS r - i color
-'''
+	'''
+		Usage: g_Grp(gr=sdss_gr_color)
+		
+		at least one color has to be set up.
+		
+		Input:
+		gr: SDSS g - r color
+		gi: SDSS g - i color
+		gz: SDSS g - z color
+		ri: SDSS r - i color
+	'''
 	return getColorPos(gr, gi, gz, ri, 6)
 
 def Gbp_Grp(gr=None, gi=None, gz=None, ri=None):
-'''
-	Usage: G_g(gr=sdss_gr_color)
-	
-	at least one color has to be set up.
-	
-	Input:
-	gr: SDSS g - r color
-	gi: SDSS g - i color
-	gz: SDSS g - z color
-	ri: SDSS r - i color
-'''
+	'''
+		Usage: Gbp_Grp(gr=sdss_gr_color)
+		
+		at least one color has to be set up.
+		
+		Input:
+		gr: SDSS g - r color
+		gi: SDSS g - i color
+		gz: SDSS g - z color
+		ri: SDSS r - i color
+	'''
 	return getColorPos(gr, gi, gz, ri, 7)
 
 
 def getColorPos(gr, gi, gz, ri, pos):
-'''
-	Returns the Gaia coded color for the position pos.
-
-'''
+	'''
+		Returns the Gaia coded color for the position pos.
+	
+	'''
 	vec = {"gr":gr, "gi":gi, "gz":gz, "ri":ri}
 
 	for i in vec.keys():
@@ -147,27 +147,32 @@ def getColorPos(gr, gi, gz, ri, pos):
 		return None
 
 def give_coefs_1_color(vec, pos):
-'''
-	Retrieves color coefficients for 1 color.
-'''
+	'''
+		Retrieves color coefficients for 1 color.
+	'''
 	col_name = vec.keys()[0]
 	col_vec = np.array(vec.values()[0])
-	dat = np.loadtxt("data/sdss_{:}.dat".format(col_name), usecols=(0, 1, 2, 3))
+	try:
+		dat = np.loadtxt("data/sdss_{:}.dat".format(col_name), usecols=(0, 1, 2, 3))
+	except:
+		print "File not found: data/sdss_{:}.dat".format(col_name)
+		return None
 	coefs = np.array(dat[pos])
 	
 	return coefs[0] + coefs[1]*col_vec + coefs[2]*col_vec**2 + coefs[3]*col_vec**3
 
 
 def give_coefs_2_color(vec, pos):
-'''
-	Retrieves color coefficients for 2 colors.
-'''
+	'''
+		Retrieves color coefficients for 2 colors.
+	'''
 	col_names = vec.keys()
 	col_vec = np.array(vec.values(), ndmin=2)
 	try:
-		dat = np.loadtxt("data/sdss_{:}_{:}.dat".format(col_names[1], col_names[0]), usecols=range(0, 8))
+		path = os.getcwd()
+		dat = np.loadtxt("{:}/data/sdss_{:}_{:}.dat".format(path,col_names[1], col_names[0]), usecols=range(0, 8))
 	except:
-		print "File not found: data/sdss_{:}_{:}.dat".format(col_names[1], col_names[0])
+		print "File not found: {:}/data/sdss_{:}_{:}.dat".format(path, col_names[1], col_names[0])
 		return None
 	coefs = np.array(dat[pos])
 
