ae1fa58a1af0c55cca3fa50a48d6ba4ae62b2a6c
Nadia Blagorodnova <nadiablago@Manolete.local>
2018-08-14 15:23:51 -0700

    Bash script which adds the import from future and replaces all the print functions in the files. It stores the result in the second file provided.

---
+++ python_2to3_converter.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+echo $0
+
+if [ "$#" -ne 2 ]; then
+    echo "Illegal number of parameters."
+    echo "Usage: python_2to3_converter.sh old_python2.X.py new_python_3.X.py"
+
+else
+    #Run the substitution for the print
+    echo "from __future__ import print_function" > $2
+    cat  $1 | sed 's/print \(.*\)/print \(\1\) /' >> $2
+fi
