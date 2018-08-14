e72a6400c16f3ab8796e85b46b024ecdc5e499ea
Nadia Blagorodnova <nadiablago@Manolete.local>
2018-08-14 15:27:20 -0700

    Added the constrain that the names have to be different.

--- python_2to3_converter.sh
+++ python_2to3_converter.sh
@@ -7,6 +7,10 @@ if [ "$#" -ne 2 ]; then
     echo "Usage: python_2to3_converter.sh old_python2.X.py new_python_3.X.py"
 
 else
+    if [ $1 = $2 ]; then
+	echo "File names shall be different"
+	exit
+    fi
     #Run the substitution for the print
     echo "from __future__ import print_function" > $2
     cat  $1 | sed 's/print \(.*\)/print \(\1\) /' >> $2
